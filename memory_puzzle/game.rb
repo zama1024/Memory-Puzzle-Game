2879fd143e8af484b69d365bc0ac613a92b75cbd
Farshid Zaman <Fzee@Macs-Air.home>
2016-12-25 16:25:32 -0500

    Make_guess created on game class

--- memory_puzzle/game.rb
+++ memory_puzzle/game.rb
@@ -2,8 +2,34 @@ require_relative "board"
 require_relative "card"
 
 class Game
+  attr_reader :board
+  attr_accessor :previously_guessed_position, :first_guess
 
   def initialize(board_size)
     @board = Board.new(board_size)
-    @previouls_guessed_position = []
+    @previously_guessed_position = []
+    @first_guess = true
   end
+
+  def make_guess
+    if self.first_guess == true
+      puts "Please insert a position guess in the format => x, y"
+      pos = (gets.chomp).split(",")
+      board.reveal([pos])
+      self.first_guess == false
+      @previously_guessed_position == pos
+    else
+      puts "Please insert a position guess in the format => x, y"
+      pos = (gets.chomp).split(",")
+      board.reveal([pos])
+      if board[pos].value != board[previously_guessed_position].value
+        board.hide(pos)
+        board.hide(previously_guessed_position)
+      end
+    end
+  end
+
+
+
+
+end
