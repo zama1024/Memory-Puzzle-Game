5858b2955b6b234f2dd722a72029f5a8f877aee1
Farshid Zaman <Fzee@Macs-Air.home>
2016-12-26 15:17:09 -0500

    Refactored humanplayer class

--- memory_puzzle/HumanPlayer.rb
+++ memory_puzzle/HumanPlayer.rb
@@ -1,7 +1,4 @@
-require_relative "board"
-
 class HumanPlayer
-  attr_reader :board
   attr_accessor :previously_guessed_position, :first_guess
 
   def initialize
@@ -9,16 +6,12 @@ class HumanPlayer
     @previously_guessed_position = []
   end
 
-  def make_guess
-    if self.first_guess == true
-      puts "Please insert a position guess in the format => x, y"
-      pos = (gets.chomp).split(",")
-      @previously_guessed_position = pos
-      return pos
-    else
-      puts "Please insert a position guess in the format => x, y"
-      pos = (gets.chomp).split(",")
-      return pos
-    end
+  def prompt
+    puts "Please insert a position guess in the format => x, y"
   end
+
+  def get_input
+    pos = (gets.chomp).split(",")
+  end
+
 end
